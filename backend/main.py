dacd6141ebd1668084b5ba3bb441830ee7fc9acd
bg20061220 <goel.bhavya020@gmail.com>
2026-02-10 18:58:02 -0500

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -27,9 +27,15 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://127.0.0.1:3000",
+<<<<<<< Updated upstream
         "https://resumetailorai.vercel.app",
     ],
     allow_origin_regex=r"https://resumetailorai-[a-z0-9]+-bhavya-goels-projects\.vercel\.app",
+=======
+        "https://tailorcvai.vercel.app",
+    ],
+    allow_origin_regex=r"https://tailorcvai-[a-z0-9]+-bhavya-goels-projects\.vercel\.app",
+>>>>>>> Stashed changes
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE", "HEAD"],
     allow_headers=["Content-Type", "Authorization"],
